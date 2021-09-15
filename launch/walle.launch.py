df4e7b003d5edd82693aedc1a53c28e349869618
Aryaman <aryashardul2002@gmail.com>
2021-09-15 11:11:40 +0530

    Added some launch files to launch world in gazebo

--- launch/walle.launch.py
+++ launch/walle.launch.py
@@ -1,27 +1,29 @@
-import launch
-from launch import actions
-from launch.substitutions import Command, LaunchConfiguration
-import launch_ros
 import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='my_bot').find('my_bot')
-    world_path=os.path.join(pkg_share, 'worlds/white.world')
 
-    return launch.LaunchDescription([
-       launch_ros.actions.Node(package='my_bot', executable = 'world', output = 'screen'),
-        
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                            description='Flag to enable use_sim_time'),
-        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
+    pkg_gazebo_ros = get_package_share_directory('my_bot')
+    pkg_my_bot = get_package_share_directory('my_bot')
 
-    ])
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    )
 
+    return LaunchDescription([
+        DeclareLaunchArgument(
+          'world',
+          default_value=[os.path.join(pkg_my_bot, 'worlds', 'white.world'), ''],
+          description=' world file'),
 
-    
-    
-    
-    
-    
-
+        gazebo,
+])
